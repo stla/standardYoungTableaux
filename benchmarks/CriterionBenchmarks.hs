4e0433ef477fbba1a5c913b1bf143cf2c1e5e2de
stla <laurent_step@yahoo.fr>
2021-01-06 03:34:39 +0100

    benchmarks

--- benchmarks/CriterionBenchmarks.hs
+++ benchmarks/CriterionBenchmarks.hs
@@ -1,12 +1,16 @@
 module Main where
-import Criterion.Main
-import Criterion.Types
-import Enumeration
+import           Criterion.Main
+import           Criterion.Types
+import           Enumeration
+import           Math.Combinat.Partitions       ( Partition(..) )
+import qualified Math.Combinat.Tableaux        as T
+
 
 main :: IO ()
 main = defaultMainWith
-  defaultConfig { resamples = 10 }
-  [ bench "syt"  $ nf standardYoungTableaux [5,3,2]
+  defaultConfig { resamples = 3 }
+  [ bench "syt" $ nf standardYoungTableaux [7, 4, 3, 2]
+  , bench "combinat" $ nf T.standardYoungTableaux (Partition [7, 4, 3, 2])
   ]
 
 -- stack bench standardYoungTableaux:criterion-benchmarks
