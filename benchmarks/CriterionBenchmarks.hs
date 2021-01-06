cb0d88746e28ec8e9d0bd4e46ed32e17a082e998
stla <laurent_step@yahoo.fr>
2021-01-06 04:44:17 +0100

    improved algorithm

--- benchmarks/CriterionBenchmarks.hs
+++ benchmarks/CriterionBenchmarks.hs
@@ -9,8 +9,9 @@ import qualified Math.Combinat.Tableaux        as T
 main :: IO ()
 main = defaultMainWith
   defaultConfig { resamples = 3 }
-  [ bench "syt" $ nf standardYoungTableaux [7, 4, 3, 2]
-  , bench "combinat" $ nf T.standardYoungTableaux (Partition [7, 4, 3, 2])
+  [ bench "syt" $ nf standardYoungTableaux [6, 6, 2, 2, 1]
+  , bench "ballot" $ nf ballotSequences [6, 6, 2, 2, 1]
+  , bench "combinat" $ nf T.standardYoungTableaux (Partition [6, 6, 2, 2, 1])
   ]
 
 -- stack bench standardYoungTableaux:criterion-benchmarks
--- src/Enumeration.hs
+++ src/Enumeration.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Enumeration
   ( ballotSequences
   , standardYoungTableaux
@@ -13,12 +14,11 @@ import           Data.Sequence                  ( Seq
                                                 )
 import qualified Data.Sequence                 as S
 
-f1 :: Seq Int -> (Int, Seq Int)
-f1 a = go (-1) lambda0 1 False
+f1 :: Int -> Seq Int -> (Int, Seq Int)
+f1 n a = go (-1) lambda0 1 False
  where
-  n       = S.length a
   lambda0 = 1 <| S.replicate (n - 1) 0
-  go isave lambda i descent
+  go !isave !lambda i !descent
     | i == n || descent
     = (isave, lambda)
     | otherwise
@@ -27,12 +27,11 @@ f1 a = go (-1) lambda0 1 False
           in  let dsct = a_i < a `index` (i - 1)
               in  let j = if dsct then i else isave in go j lmbd (i + 1) dsct
 
-f2 :: Seq Int -> Seq Int -> Int -> (Seq Int, Seq Int, Int)
-f2 a0 lambda0 isave = go (n - 1) a0 lambda0 it0
+f2 :: Int -> Seq Int -> Seq Int -> Int -> (Seq Int, Seq Int, Int)
+f2 n a0 lambda0 isave = go (n - 1) a0 lambda0 it0
  where
-  n   = S.length a0
   it0 = lambda0 `index` ((a0 `index` isave) + 1)
-  go i a lambda it
+  go i !a !lambda !it
     | i == (-1)
     = (a, lambda, it)
     | lambda `index` i == it
@@ -40,11 +39,10 @@ f2 a0 lambda0 isave = go (n - 1) a0 lambda0 it0
     | otherwise
     = go (i - 1) a lambda it
 
-f3 :: Seq Int -> Seq Int -> Int -> Seq Int
-f3 a0 lambda0 it = go 0 0 a0 lambda0
+f3 :: Int -> Seq Int -> Seq Int -> Int -> Seq Int
+f3 n a0 lambda0 it = go 0 0 a0 lambda0
  where
-  n = S.length a0
-  go k ir a lambda
+  go k ir !a !lambda
     | n - 1 < ir = a
     | lambda `index` ir /= 0 = go (k + 1)
                                   (ir + 1)
@@ -60,32 +58,32 @@ descent a = go 0
   go i = i < n - 1 && (a `index` (i + 1) < a `index` i || go (i + 1)) 
 -}
 
-ballot :: Seq Int -> Seq Int -> Bool -> (Seq Int, Bool)
-ballot a0 lambda0 more =
+ballot :: Int -> Seq Int -> Seq Int -> Bool -> (Seq Int, Bool)
+ballot n a0 lambda0 more =
   let l = S.length lambda0
-  in  let (isave, lambda1) = f1 a0
-      in  let (a1, lambda2, it0) = f2 a0 lambda1 isave
+  in  let (isave, lambda1) = f1 n a0
+      in  let (a1, lambda2, it0) = f2 n a0 lambda1 isave
           in  let (it, a3, lambda3) = if more
                     then (it0, a1, lambda2)
                     else (n, a0, lambda0 >< S.replicate (n - l) 0)
               in  if more && isave == -1
                     then (a1, False)
                     else
-                      let a = f3 a3 lambda3 it
+                      let a = f3 n a3 lambda3 it
                       in  if n == 1 then (a, True) else (a, descent a)
  where
-  n = S.length a0
   descent b = go 0
     where go i = i < n - 1 && (b `index` (i + 1) < b `index` i || go (i + 1))
 
 ballotSequences :: [Int] -> [Seq Int]
 ballotSequences lambda0 =
-  let a0 = S.replicate (sum lambda0) 0
-  in  let (a, more) = ballot a0 lambda False in go [a] more
+  let a0 = S.replicate n 0
+  in  let (a, more) = ballot n a0 lambda False in go [a] more
  where
+  n = sum lambda0
   lambda = S.fromList lambda0
-  go list mr = if mr
-    then let (a', mr') = ballot (head list) lambda True in go (a' : list) mr'
+  go !list !mr = if mr
+    then let (a', mr') = ballot n (head list) lambda True in go (a' : list) mr'
     else list
 
 ballot2syt :: Seq Int -> Seq (Seq Int)
@@ -93,11 +91,20 @@ ballot2syt a = go voidsyt 0
  where
   voidsyt = S.replicate (maximum a + 1) (S.empty :: Seq Int)
   n       = S.length a
-  go syt i
+  go !syt i
     | i == n    = syt
     | otherwise = go (update a_i ((syt `index` a_i) |> (i + 1)) syt) (i + 1)
     where a_i = a `index` i
 
 standardYoungTableaux :: [Int] -> [Seq (Seq Int)]
-standardYoungTableaux lambda = map ballot2syt (ballotSequences lambda)
+standardYoungTableaux lambda = map tosyt (ballotSequences lambda)
+ where
+  tosyt a = go voidsyt 0
+   where
+    voidsyt = S.replicate (maximum a + 1) (S.empty :: Seq Int)
+    n       = sum lambda
+    go !syt i
+      | i == n    = syt
+      | otherwise = go (update a_i ((syt `index` a_i) |> (i + 1)) syt) (i + 1)
+      where a_i = a `index` i
 
